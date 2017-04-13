a43e7a1f6eca2fa43e484706facdd10fe9b64964
Jd Fiscus <me@iamfiscus.com>
2017-04-12 23:01:15 -0500

    fix(install): find and replace bug

--- bin/install.sh
+++ bin/install.sh
@@ -2,19 +2,18 @@
 PROJECT="freeboard-aws-iot-ws-mqtt"
 pwd
 if [ -z "$1" ]; then
-  PROJECT_PATH="../../plugins"
+  PROJECT_LOCATION="../../"
+  PROJECT_PATH="plugins"
 
-  if [ ! -d "$PROJECT_PATH" ]; then
+  if [ ! -d "$PROJECT_LOCATION$PROJECT_PATH" ]; then
     echo 'Must be in a valid Freeboard project'
     exit
   fi
-
+  PROJECT_FULL_PATH="${PROJECT_LOCATION}${PROJECT_PATH}/${PROJECT}"
 else
   PROJECT_PATH=$1
+  PROJECT_FULL_PATH="${PROJECT_PATH}/${PROJECT}"
 fi
-
-
-PROJECT_FULL_PATH="${PROJECT_PATH}/${PROJECT}"
 
 if [ ! -d "$PROJECT_FULL_PATH" ]; then
   mkdir -p $PROJECT_FULL_PATH
@@ -29,9 +28,9 @@ cp node_modules/crypto-js/sha256.js out/sha256.js
 cp node_modules/moment/min/moment.min.js out/moment.min.js
 cp node_modules/paho-mqtt/mqttws31-min.js out/mqttws31-min.js
 cp src/index.js out/index.js
-
-sed -i -e "s:plugins:${PROJECT_PATH}:g" out/index.js
-
+if [ -z "$1" ]; then
+  sed -i -e "s:plugins:${PROJECT_PATH}:g" out/index.js
+fi
 rm out/index.js-e
 
 cp -R out/* $PROJECT_FULL_PATH
